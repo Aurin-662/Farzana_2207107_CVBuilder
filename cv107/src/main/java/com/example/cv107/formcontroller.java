cbb0c3c9f18bf25cdad4e55642e8c6ccd73ea8e8
Aurin-662 <aurin662@gmail.com>
2025-11-17 16:39:33 +0600

    added generate button functionalities

--- cv107/src/main/java/com/example/cv107/formcontroller.java
+++ cv107/src/main/java/com/example/cv107/formcontroller.java
@@ -1,4 +1,33 @@
 package com.example.cv107;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class formcontroller {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+
+    @FXML
+    public void generate(ActionEvent event) throws IOException {
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("preview.fxml"));
+        root = loader.load();
+
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
