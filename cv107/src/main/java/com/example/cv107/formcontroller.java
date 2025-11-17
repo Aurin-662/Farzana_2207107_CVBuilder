6cec1fbaeb174eac3026f2ad8c9fe617a593f995
Aurin-662 <aurin662@gmail.com>
2025-11-17 21:21:41 +0600

    updated controller classes

--- cv107/src/main/java/com/example/cv107/formcontroller.java
+++ cv107/src/main/java/com/example/cv107/formcontroller.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,14 +17,51 @@ public class formcontroller {
     private Stage stage;
     private Scene scene;
 
+    @FXML
+    TextField user;
+    @FXML
+    TextField email;
+    @FXML
+    TextField mobile;
+    @FXML
+    TextField address;
+    @FXML
+    TextField education;
+    @FXML
+    TextField projects;
+    @FXML
+    TextField skills;
+    @FXML
+    TextField ex;
+    @FXML
+    TextArea ab;
 
     @FXML
     public void generate(ActionEvent event) throws IOException {
+        String name1=user.getText();
+        String email1=email.getText();
+        String mb=mobile.getText();
+        String ad=address.getText();
+        String ed=education.getText();
+        String pro=projects.getText();
+        String sk=skills.getText();
+        String exx=ex.getText();
+        String abb=ab.getText();
 
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("preview.fxml"));
         root = loader.load();
 
+        previewcontroller Sc3=loader.getController();
+        Sc3.displayy(name1);
+        Sc3.displayy2(email1);
+        Sc3.displayy3(mb);
+        Sc3.displayy4(ad);
+        Sc3.displayy5(ed);
+        Sc3.displayy6(pro);
+        Sc3.displayy7(sk);
+        Sc3.displayy8(exx);
+        Sc3.displayy9(abb);
 
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
--- cv107/src/main/java/com/example/cv107/previewcontroller.java
+++ cv107/src/main/java/com/example/cv107/previewcontroller.java
@@ -1,4 +1,54 @@
 package com.example.cv107;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class previewcontroller {
+    @FXML
+    Label myname;
+    @FXML
+    Label mail;
+    @FXML
+    Label phone;
+    @FXML
+    Label add;
+    @FXML
+    Label edu;
+    @FXML
+    Label pro;
+    @FXML
+    Label skil;
+    @FXML
+    Label work;
+    @FXML
+    Label about;
+
+    public void displayy(String name){
+        myname.setText( name);
+    }
+    public void displayy2(String name2){
+        mail.setText( name2);
+    }
+    public void displayy3(String name3){
+        phone.setText( name3);
+    }
+    public void displayy4(String name4){
+        add.setText( name4);
+    }
+
+    public void displayy5(String name5){
+        edu.setText( name5);
+    }
+    public void displayy6(String name6){
+        pro.setText( name6);
+    }
+    public void displayy7(String name7){
+        skil.setText( name7);
+    }
+    public void displayy8(String name8){
+        work.setText( name8);
+    }
+    public void displayy9(String name9){
+        about.setText( name9);
+    }
 }
